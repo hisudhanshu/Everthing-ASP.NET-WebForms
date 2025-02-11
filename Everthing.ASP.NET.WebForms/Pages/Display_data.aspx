8b8e054c545fca574be92fcfeef60d033db48c8d
Sudhanshu Bharti <105193952+hisudhanshu@users.noreply.github.com>
2025-02-11 14:55:50 +0530

    display data show in gridview is done

---
+++ Everthing.ASP.NET.WebForms/Pages/Display_data.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Display_data.aspx.cs" Inherits="Everthing.ASP.NET.WebForms.Pages.Display_data" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div>
+        <h2>Display All Inserted Data</h2>
+
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="true" BorderColor="Black" BorderWidth="1px" Width="100%">
+        </asp:GridView>
+    </div>
+</asp:Content>
---
+++ Everthing.ASP.NET.WebForms/Pages/Display_data.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Everthing.ASP.NET.WebForms.Pages
+{
+    public partial class Display_data : System.Web.UI.Page
+    {
+        // ✅ Connection String from Web.config
+        string connString = WebConfigurationManager.ConnectionStrings["database_aspwebforms"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadData();
+            }
+        }
+
+        // ✅ Load Data Method to Fetch and Display Data in GridView
+        private void LoadData()
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT Id, Name, LastName, Email, Dob, CreatedDate FROM Everything", conn);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+        }
+    }
+}

67cd28d9ae6cc27ebb161f44b7b34b8a2b2ef413
Sudhanshu Bharti <105193952+hisudhanshu@users.noreply.github.com>
2025-02-11 15:37:32 +0530

    data display by using VIEW

---
+++ Everthing.ASP.NET.WebForms/Pages/Display_data_from_view.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Display_data_from_view.aspx.cs" Inherits="Everthing.ASP.NET.WebForms.Pages.Display_data_from_view" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="True"
+        CssClass="table" BorderColor="#DEDFDE"
+        ForeColor="#333333"
+        Width="100%"
+        AllowPaging="True"
+        PageSize="10">
+    </asp:GridView>
+</asp:Content>
---
+++ Everthing.ASP.NET.WebForms/Pages/Display_data_from_view.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Everthing.ASP.NET.WebForms.Pages
+{
+    public partial class Display_data_from_view : System.Web.UI.Page
+    {
+        // ✅ Connection String from Web.config
+        string connString = WebConfigurationManager.ConnectionStrings["database_aspwebforms"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadData();
+            }
+        }
+     private void LoadData()
+        {
+            // Define the connection string
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["database_aspwebforms"].ToString();
+
+            // Create SQL query to get data from DataView
+            string query = "SELECT * FROM DataView";
+
+            // Create a connection and command object
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+
+                // Fill the data table with the query result
+                da.Fill(dt);
+
+                // Bind the data to the GridView
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+        }
+    }
+}
